b5106657914fd5061200498c50be176cd502a589
harshcad01 <harshcad01@gmail.com>
2024-02-21 17:55:49 +0530

    c

---
+++ day2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Bread
+{
+    public string bre = "I use to eat Bread"; 
+}
+public class Patty:Bread
+{
+    public string pat= "And also like to have Patty";
+}
+
+class Test
+{
+    public static void Main(string [] args){
+    Patty p= new Patty();
+
+    Console.WriteLine("Harsh :" + p.bre);
+    Console.WriteLine("Arav :" +p.pat);
+}
+}
