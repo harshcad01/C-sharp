7da0b1821d5bcbe9f06ae391fa5b25d8550d1af9
harshcad01 <harshcad01@gmail.com>
2024-02-21 23:27:15 +0530

    c

--- day2/Program.cs
+++ day2/Program.cs
@@ -2,19 +2,20 @@
 
 public class Bread
 {
-    public string bre = "I use to eat Bread"; 
+    public string bre = "I use to eat Bread";
 }
-public class Patty:Bread
+public class Patty : Bread
 {
-    public string pat= "And also like to have Patty";
+    public string pat = "And also like to have Patty";
 }
 
 class Test
 {
-    public static void Main(string [] args){
-    Patty p= new Patty();
+    public static void Main(string[] args)
+    {
+        Patty p = new Patty();
 
-    Console.WriteLine("Harsh :" + p.bre);
-    Console.WriteLine("Arav :" +p.pat);
-}
+        Console.WriteLine("Harsh :" + p.bre);
+        Console.WriteLine("Arav :" + p.pat);
+    }
 }
